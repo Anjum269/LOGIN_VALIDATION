ff2bdebaa5e59739d71c96571a37ad985c13952d
Anjum269 <01fe24bca044.kletech@gmail.com>
2025-11-19 11:09:18 +0530

    Refactor login validation to simplify default values

--- login_validation.py
+++ login_validation.py
@@ -1,14 +1,13 @@
 import sys
 
 print("login validation program")
-
-# If arguments are given
+ 
 if len(sys.argv) == 4:
     mobile = sys.argv[1]
     email = sys.argv[2]
     password = sys.argv[3]
 else:
-    # default values for Jenkins
+     
     mobile = "9999999999"
     email = "default@gmail.com"
     password = "admin123"
