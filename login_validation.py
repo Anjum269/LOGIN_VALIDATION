33bb1330b0fcd4097a4e59b771505bdbde36241e
Anjum <anjumkousar079@gmail.com>
2025-11-19 09:56:58 +0530

    save

---
+++ login_validation.py
@@ -0,0 +1,17 @@
+import sys
+if len(sys.argv) != 4:
+    print("Usage: python login_validation.py <mobile> <email> <password>")
+    sys.exit()
+    mobile = sys.argv[1]
+    email = sys.argv[2] 
+    password = sys.argv[3]
+    print("login validation program")
+    #validation
+    valid_mobile = "1234567890"
+    valid_email = "user@gmail.com"
+    valid_password = "admin123"
+    if (mobile == valid_mobile and email == valid_email and password == valid_password):
+        print("Login Successful") 
+    else:
+        print("Invalid Login credientials")
+        
